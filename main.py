820e87ae0dd4fc77568c2dc5e7407bca4211613e
Rich <Richardmcollado@gmail.com>
2023-02-01 17:50:20 -0500

    Render app titles in two columns

---
+++ main.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas
+
+st.set_page_config(layout="wide")
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.jpg")
+
+with col2:
+    st.title("Rich Collado")
+    content = """
+    Good day, I'm Rich, an IT specialist, and all around awsome fella. 
+    Since 2003, I have been working with databases, networking equipment, 
+    web development, and programming.
+    """
+    st.info(content)
+
+content_2 = """
+Below you can find some of the apps I have built in python. Feel free to contact me!
+"""
+st.write(content_2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+
